a44c3dd885b064b9cdf3b949e48fe3b34d75e5d1
An Nguyen <nguanv@amazon.com>
2023-11-14 19:29:41 -0800

    Added some validation

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
+const Joi = require('joi');
 const Salon = require('./models/salon')
 
 mongoose.set('strictQuery', true);
@@ -22,6 +25,23 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
+const validateCampground = (req, res, next) => {
+    const salonSchema = Joi.object({
+        salon: Joi.object({
+            name: Joi.string().required,
+            description: Joi.string().required,
+            location: Joi.string().required
+        }).required()
+    })
+    const {error} = salonSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
 app.get('/',(req,res) => {
     res.render('home')
 })
@@ -34,33 +54,50 @@ app.get('/salons/new', (req, res) => {
     res.render('salons/new');
 })
 
-app.post('/salons', async (req, res) => {
-    const salon = new Salon(req.body.salon);
-    await salon.save();
-    res.redirect(`/salons/${salon._id}`)
-})
+app.post('/salons', validateCampground, catchAsync(async (req, res, next) => {
+        // if(!req.body.salon) throw new ExpressError('Invalid Salon Data', 400)
+        
+        const salon = new Salon(req.body.salon);
+        await salon.save();
+        res.redirect(`/salons/${salon._id}`)
+    
+}))
 
-app.get('/salons/:id', async(req, res) => {
+app.get('/salons/:id', catchAsync(async(req, res) => {
     const salon  = await  Salon.findById(req.params.id)
     res.render('salons/show', {salon})
-})
+}))
 
-app.get('/salons/:id/edit', async (req,res) => {
+app.get('/salons/:id/edit', catchAsync(async (req,res) => {
     const salon  = await Salon.findById(req.params.id)
     res.render('salons/edit', {salon})
-})
+}))
 
-app.put('/salons/:id', async (req, res) => {
+app.put('/salons/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const salon = await Salon.findByIdAndUpdate(id,{...req.body.salon});
     res.redirect(`/salons/${salon._id}`)
-})
+}))
 
-app.delete('/salons/:id', async (req, res) => {
+app.delete('/salons/:id', catchAsync(async (req, res) => {
     const {id} = req.params;
     await Salon.findByIdAndDelete(id);
     res.redirect('/salons')
+}))
+
+
+
+// Error handling
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Note Found', 404))
 })
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = "Something Went Wrong"} = err
+    if (!err.message) err.message = 'Oh no, something went wrong'
+    res.status(statusCode).render('error', {err})
+})
+
 
 app.listen(3000, () => {
     console.log('serving on port 3000')
