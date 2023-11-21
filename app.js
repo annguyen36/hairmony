9833c1e00ded0022cf9be80233aa19d615a839ef
An Nguyen <nguanv@amazon.com>
2023-11-20 18:26:48 -0800

    Added reviews mechanism

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride = require('method-override');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const Joi = require('joi');
-const Salon = require('./models/salon')
+const Salon = require('./models/salon');
+const Review = require('./models/review');
 
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://127.0.0.1:27017/hairmony')
@@ -25,15 +26,31 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
-const validateCampground = (req, res, next) => {
+const validateSalon = (req, res, next) => {
     const salonSchema = Joi.object({
         salon: Joi.object({
-            name: Joi.string().required,
-            description: Joi.string().required,
-            location: Joi.string().required
+            name: Joi.string().required(),
+            description: Joi.string().required(),
+            location: Joi.string().required(),
+            image: Joi.string().required()
         }).required()
     })
     const {error} = salonSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+const validateReview = (req, res, next) => {
+    const reviewSchema = Joi.object({
+        review: Joi.object({
+            rating: Joi.number().required().min(1).max(5),
+            body: Joi.string().required()
+        }).required()
+    })
+    const {error} = reviewSchema.validate(req.body);
     if(error){
         const msg = error.details.map(el => el.message).join(',')
         throw new ExpressError(msg, 400)
@@ -54,7 +71,7 @@ app.get('/salons/new', (req, res) => {
     res.render('salons/new');
 })
 
-app.post('/salons', validateCampground, catchAsync(async (req, res, next) => {
+app.post('/salons', validateSalon, catchAsync(async (req, res, next) => {
         // if(!req.body.salon) throw new ExpressError('Invalid Salon Data', 400)
         
         const salon = new Salon(req.body.salon);
@@ -64,7 +81,7 @@ app.post('/salons', validateCampground, catchAsync(async (req, res, next) => {
 }))
 
 app.get('/salons/:id', catchAsync(async(req, res) => {
-    const salon  = await  Salon.findById(req.params.id)
+    const salon  = await  Salon.findById(req.params.id).populate('reviews')
     res.render('salons/show', {salon})
 }))
 
@@ -85,7 +102,21 @@ app.delete('/salons/:id', catchAsync(async (req, res) => {
     res.redirect('/salons')
 }))
 
+app.post('/salons/:id/reviews', validateReview, catchAsync(async (req, res) => {
+    const salon = await Salon.findById(req.params.id);
+    const review = new Review(req.body.review);
+    salon.reviews.push(review);
+    await review.save();
+    await salon.save();
+    res.redirect(`/salons/${salon._id}`)
+}))
 
+app.delete('/salons/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Salon.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+    await Review.findByIdAndDelete(req.params.reviewId);
+    res.redirect(`/salons/${id}`)
+}))
 
 // Error handling
 
--- models/salon.js
+++ models/salon.js
@@ -1,12 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review')
 
 const haircutSchema = new Schema({
     name: String,
     image: String,
     price: String,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
 
+haircutSchema.post('findOneAndDelete', async function (params) {
+    if(params){
+        await Review.deleteMany({
+            _id: {
+                $in: params.reviews
+            }
+        })
+    }
+})
 module.exports = mongoose.model('Haircut', haircutSchema);
